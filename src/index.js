7a0e5e3a7a0bd58e9e1f783aa57bc55dbfbea05d
Haroon Ilyas <haroonilyas14@yahoo.com>
2019-08-04 22:30:15 +0500

    Second Iteration

--- src/index.js
+++ src/index.js
@@ -75,27 +75,27 @@ class Buttons extends React.Component {
 
                 <ButtonToolbar>
 
-                    <button className="btn btn-default" onClick={this.props.playButton}>
+                    <button className="btn btn-light mr-1" onClick={this.props.playButton}>
                         Play
                     </button>
 
-                    <button className="btn btn-default" onClick={this.props.pauseButton}>
+                    <button className="btn btn-light mr-1" onClick={this.props.pauseButton}>
                         Pause
                     </button>
 
-                    <button className="btn btn-default" onClick={this.props.clear}>
+                    <button className="btn btn-light mr-1" onClick={this.props.clear}>
                         Clear
                     </button>
 
-                    <button className="btn btn-default" onClick={this.props.slow}>
+                    <button className="btn btn-light mr-1" onClick={this.props.slow}>
                         Slow
                     </button>
 
-                    <button className="btn btn-default" onClick={this.props.fast}>
+                    <button className="btn btn-light mr-1" onClick={this.props.fast}>
                         Fast
                     </button>
 
-                    <button className="btn btn-default" onClick={this.props.seed}>
+                    <button className="btn btn-light mr-1" onClick={this.props.seed}>
                         Seed
                     </button>
 
